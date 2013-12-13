f4633c835c10749d2e4bfbfd09d741c7c577265e
Tasha25 <greennatasha@aol.com>
2013-12-13 13:36:16 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weintervene::Application.config.secret_token = 'edcc92b4ec61005c928220b7cc200a519b97e329ec9cbe2965da4871c4388f6f75e19590c2c215159f3f683c7132c91344dc5881adabb1c25bc2164ed245e47e'
