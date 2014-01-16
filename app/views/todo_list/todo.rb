f4e29edb380f2a3d5ed94254b950f96bd26cb049
Tasha25 <greennatasha@aol.com>
2014-01-16 13:29:23 -0500

    Added student name to Incident row, fixed the date in referrals

---
+++ app/views/todo_list/todo.rb
@@ -0,0 +1,29 @@
+Place logo in file to pull up off line.
+
+On user's page
+user should see a search field to lookup students
+user should have navigations to other parts of site
+user should
+
+
+
+#TODO User should be able to login
+User should be able to create a profile
+User should be able to search for a student
+User should be able to attach a referral to a student
+User should be able to search for service-providers(sps)
+User should be able to attach a service-provider to the student
+
+User should be able to see previous referrals
+User should be able to find a sp for a student according to their infractions
+
+User should be able to sort through sps by:
+location
+distance from school
+service-category
+
+User should be able to:
+- print out referral for student
+- email guardian the referral
+- email organization about reccommendation
+
