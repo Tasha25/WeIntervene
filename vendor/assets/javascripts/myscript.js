589c836475fccb79fafbe6afbee166b468a09615
Tasha25 <greennatasha@aol.com>
2014-01-23 08:46:18 -0500

    Changed User's home page

--- vendor/assets/javascripts/myscript.js
+++ vendor/assets/javascripts/myscript.js
@@ -1,10 +1,20 @@
 $(function() {
 
 
-$('ul.nav li.dropdown').hover(function() {
-  $('.dropdown-menu', this).fadeIn()
-}, function() {
-  $('.dropdown-menu', this).fadeOut('fast');
-}); //hover
+  $("#home a:contains('Home')").parent().addClass('active');
+
+
+  $('ul.nav li.dropdown').hover(function() {
+    $('.dropdown-menu', this).fadeIn()
+  }, function() {
+    $('.dropdown-menu', this).fadeOut('fast');
+  }); //hover over navigation
+
+  $('ul.mainnav li.dropdown').hover(function() {
+    $('.dropdown-menu', this).fadeIn()
+  }, function() {
+    $('.dropdown-menu', this).fadeOut('fast');
+  }); //hover over navigation
+
 
 }); //jQuery is loaded
